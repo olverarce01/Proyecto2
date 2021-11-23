eb56a235c9fcb37c567b9e3ddc7b99da90b378f5
olver jose arce limache <olver.arce.limache@alumnos.uta.cl>
2021-11-23 14:49:19 -0300

    layout camara detecta 5 tipos

--- app/src/main/java/com/example/proyecto2/CameraActivity.java
+++ app/src/main/java/com/example/proyecto2/CameraActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.graphics.Rect;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Size;
 import android.view.OrientationEventListener;
 import android.widget.TextView;
@@ -17,6 +18,7 @@ import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
@@ -32,6 +34,7 @@ import com.google.mlkit.vision.objects.defaults.PredefinedCategory;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 import javax.annotation.Nullable;
 
@@ -39,12 +42,6 @@ public class CameraActivity extends AppCompatActivity{
     private PreviewView previewView;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private TextView textView;
-    ObjectDetectorOptions options =
-            new ObjectDetectorOptions.Builder()
-                    .setDetectorMode(ObjectDetectorOptions.STREAM_MODE)
-                    .enableClassification()  // Optional
-                    .build();
-    ObjectDetector objectDetector = ObjectDetection.getClient(options);
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,84 +49,139 @@ public class CameraActivity extends AppCompatActivity{
         previewView = findViewById(R.id.previewView);
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         textView = findViewById(R.id.orientation);
+        /*
         cameraProviderFuture.addListener(new Runnable() {
             @Override
             public void run() {
                 try {
                     ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                    bindImageAnalysis(cameraProvider);
-                } catch (ExecutionException | InterruptedException e) {
+                    bindPreview(cameraProvider);
+                }catch (ExecutionException|InterruptedException e){
                     e.printStackTrace();
                 }
             }
-        }, ContextCompat.getMainExecutor(this));
+        },ContextCompat.getMainExecutor(this));*/
+        
+        cameraProviderFuture.addListener(()->{
+            try {
+                ProcessCameraProvider cameraProvider=cameraProviderFuture.get();
+                //bindPreview(cameraProvider);
+                bindImageAnalysis(cameraProvider);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        },ContextCompat.getMainExecutor(this));
     }
 
     private void bindImageAnalysis(@NonNull ProcessCameraProvider cameraProvider) {
-        ImageAnalysis imageAnalysis =
-                new ImageAnalysis.Builder().setTargetResolution(new Size(1280, 720))
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
+        cameraProvider.unbindAll();
+        Preview preview=new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        ImageAnalysis imageAnalysis=
+                new ImageAnalysis.Builder()
+                        .setTargetResolution(new Size(previewView.getWidth(),previewView.getHeight()))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
         imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
             @Override
             public void analyze(@NonNull ImageProxy imageProxy) {
-                @SuppressLint("UnsafeOptInUsageError") Image mediaImage = imageProxy.getImage();
-                if (mediaImage != null) {
-                    InputImage image =
-                            InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
-                    // Pass image to an ML Kit Vision API
-                    // ...
+                int rotationDegrees=imageProxy.getImageInfo().getRotationDegrees();
+                @SuppressLint("UnsafeOptInUsageError") Image mediaImage=imageProxy.getImage();
+                if(mediaImage!=null){
+                    InputImage image= InputImage.fromMediaImage(mediaImage,rotationDegrees);
+                    ObjectDetectorOptions options =
+                            new ObjectDetectorOptions.Builder()
+                                    .setDetectorMode(ObjectDetectorOptions.STREAM_MODE)
+                                    .enableClassification()  // Optional
+                                    .build();
+                    ObjectDetector objectDetector = ObjectDetection.getClient(options);
                     objectDetector.process(image)
-                            .addOnSuccessListener(
-                                    new OnSuccessListener<List<DetectedObject>>() {
-                                        @Override
-                                        public void onSuccess(List<DetectedObject> detectedObjects) {
-                                            // Task completed successfully
-                                            // ...
-                                            for (DetectedObject detectedObject : detectedObjects) {
-                                                Rect boundingBox = detectedObject.getBoundingBox();
-                                                Integer trackingId = detectedObject.getTrackingId();
-                                                for (DetectedObject.Label label : detectedObject.getLabels()) {
-                                                    String text = label.getText();
-                                                    if (PredefinedCategory.FOOD.equals(text)) {
-                                                    }
-                                                    int index = label.getIndex();
-                                                    if (PredefinedCategory.FOOD_INDEX == index) {
-                                                    }
-                                                    float confidence = label.getConfidence();
-                                                }
-                                            }
-                                            Toast toast1 =
-                                                    Toast.makeText(getApplicationContext(),
-                                                            "Se detectaron: ", Toast.LENGTH_LONG);
+                            .addOnSuccessListener(detectedObjects -> {
+                                for (DetectedObject detectedObject : detectedObjects) {
+                                    Rect boundingBox = detectedObject.getBoundingBox();
+                                    Integer trackingId = detectedObject.getTrackingId();
+                                    for (DetectedObject.Label label : detectedObject.getLabels()) {
+                                        String text = label.getText();
+                                        if (PredefinedCategory.FOOD.equals(text)) {
+                                        }
+                                        int index = label.getIndex();
+                                        if (PredefinedCategory.FOOD_INDEX == index) {
+                                        }
+                                        float confidence = label.getConfidence();
+                                        Log.d("xd", "objeto: "+label.getText()+" index: "+label.getIndex()+" confidence:"+label.getConfidence());
+                                    }
 
-                                            toast1.show();
-                                        }
-                                    })
-                            .addOnFailureListener(
-                                    new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            // Task failed with an exception
-                                            // ...
-                                        }
-                                    });
-                }imageProxy.close();
+                                }
+
+                                //Log.d("xd", "#objetos: "+String.valueOf(detectedObjects.size()));
+                                //Log.d("TAG", "onSuccess" + detectedObjects.size());
+                            })
+                            .addOnFailureListener(e -> Log.e("TAG", e.getLocalizedMessage()))
+                            .addOnCompleteListener(result -> imageProxy.close());
+                }//imageProxy.close();
+
             }
         });
-        OrientationEventListener orientationEventListener = new OrientationEventListener(this) {
-            @Override
-            public void onOrientationChanged(int orientation) {
-                textView.setText(Integer.toString(orientation));
-            }
-        };
-        orientationEventListener.enable();
-        Preview preview = new Preview.Builder().build();
-        CameraSelector cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
-        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector,
-                imageAnalysis, preview);
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector,imageAnalysis,preview);
     }
 
+/*
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+        Preview preview=new Preview.Builder().build();
 
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        ImageAnalysis imageAnalysis=
+                new ImageAnalysis.Builder()
+                .setTargetResolution(new Size(previewView.getWidth(),previewView.getHeight()))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
+            @Override
+            public void analyze(@NonNull ImageProxy imageProxy) {
+                int rotationDegrees=imageProxy.getImageInfo().getRotationDegrees();
+                @SuppressLint("UnsafeOptInUsageError") Image mediaImage=imageProxy.getImage();
+                if(mediaImage!=null){
+                    InputImage image= InputImage.fromMediaImage(mediaImage,rotationDegrees);
+                    ObjectDetectorOptions options =
+                            new ObjectDetectorOptions.Builder()
+                                    .setDetectorMode(ObjectDetectorOptions.STREAM_MODE)
+                                    .enableClassification()  // Optional
+                                    .build();
+                    ObjectDetector objectDetector = ObjectDetection.getClient(options);
+                    objectDetector.process(image)
+                            .addOnSuccessListener(new OnSuccessListener<List<DetectedObject>>() {
+                                @Override
+                                public void onSuccess(@NonNull List<DetectedObject> detectedObjects) {
+                                    for (DetectedObject detectedObject : detectedObjects) {
+                                        Rect boundingBox = detectedObject.getBoundingBox();
+                                        Integer trackingId = detectedObject.getTrackingId();
+                                        for (DetectedObject.Label label : detectedObject.getLabels()) {
+                                            String text = label.getText();
+                                            if (PredefinedCategory.FOOD.equals(text)) {
+                                            }
+                                            int index = label.getIndex();
+                                            if (PredefinedCategory.FOOD_INDEX == index) {
+                                            }
+                                            float confidence = label.getConfidence();
+                                        }
+                                    }
+                                    Log.d("xd", String.valueOf(detectedObjects.size()));
+                                }
+                            });
+                }imageProxy.close();
+
+            }
+        });
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        cameraProvider.bindToLifecycle((LifecycleOwner) this,cameraSelector,preview,imageAnalysis);
+    }*/
 }
